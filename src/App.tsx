992ee78ce86ff02e3b0d57a03784343ae9125052
brouk <cdbrouk@gmail.com>
2020-04-20 15:43:53 +0900

    Apply NanumGothic google font

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <p>idus frontend</p>
     </div>
   );
---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+const fontURL = require('../assets/fonts/NanumGothic-Regular.ttf');
+
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'NanumGothic';
+    src: url(${fontURL});
+  }
+  body {
+    font-family: 'NanumGothic', consolas;
+  }
+`;
+
+export default GlobalStyles;
