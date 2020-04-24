d3790523bb17d2c076231ac31e0075cd422951f3
brouk <cdbrouk@gmail.com>
2020-04-24 13:02:39 +0900

    Updates

---
+++ precache-manifest.167d201c617a88b2517e8bdfe5ba2aa9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aaff27f9aa911af26b974580e7752e7",
+    "url": "/idus-front/index.html"
+  },
+  {
+    "revision": "9d225af4f6b59928712d",
+    "url": "/idus-front/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3fb4f1d080a6388f32d8",
+    "url": "/idus-front/static/js/2.f1178860.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/idus-front/static/js/2.f1178860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d225af4f6b59928712d",
+    "url": "/idus-front/static/js/main.8eaeba10.chunk.js"
+  },
+  {
+    "revision": "9741859d9dae85ecfe48",
+    "url": "/idus-front/static/js/runtime-main.b1241c83.js"
+  },
+  {
+    "revision": "3cbe9257e888255cb2dad0d8a80a7759",
+    "url": "/idus-front/static/media/NanumGothic-Regular.3cbe9257.ttf"
+  }
+]);
